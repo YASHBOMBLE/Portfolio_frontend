f46e1340b8dd533194d138f78d7c85d7c8fb3389
YASHBOMBLE <yashbomble2002@gmail.com>
2026-07-12 10:03:54 +0530

    first commit

--- js/contact.js
+++ js/contact.js
@@ -5,7 +5,7 @@
 ===================================================== */
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:5000/api'
-  : 'http://localhost:5000/api';
+  : 'https://portfolio-backend-gbzi.onrender.com/api';
 
 /* =====================================================
    CONTACT FORM
